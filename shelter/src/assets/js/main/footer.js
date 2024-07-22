4319ed0e6d6bcebc282c8fd6587bbcef29de27df
anthony-th <yefremov.anthony@gmail.com>
2024-07-23 00:16:21 +0300

    refactor: reorganization about section and upd styles for section (Tue, Jul 23, 2024 12:16:20 AM)

--- shelter/src/assets/js/main/footer.js
+++ shelter/src/assets/js/main/footer.js
@@ -16,7 +16,7 @@ const footerLocations = createElement('div', ['locations']);
 const footerLocationsTitle = createElement('h3', ['locations-title'], 'We are waiting for your visit');
 const footerLocationsBoston = createLocation('https://www.google.com/maps/place/1+Central+St,+Boston,+MA+02109,+%D0%A1%D0%A8%D0%90/@42.3585558,-71.056915,17z/data=!3m1!4b1!4m5!3m4!1s0x89e370868bc2ce7b:0x82fa7db94f5fea9e!8m2!3d42.3585519!4d-71.0547263', locationImgSrc, '1 Central Street, Boston (entrance from the store)', ['location-boston', 'location-boston-img', 'location-boston-text']);
 const footerLocationsLondon = createLocation('https://www.google.com/maps/place/18+South+Rd,+Londonderry,+NH+03053,+%D0%A1%D0%A8%D0%90/@42.8246256,-71.364361,17z/data=!3m1!4b1!4m5!3m4!1s0x89e3ad7442d3b7b7:0x5550ec16f1a4554d!8m2!3d42.8246217!4d-71.3621723', locationImgSrc, '18 South Park, London', ['location-london', 'location-london-img', 'location-london-text']);
-const footerImg = createElement('img', ['footer-img'], '', { src: footerImgSrc });
+const footerImg = createElement('img', ['footer-img'], '', { src: footerImgSrc, alt: '' });
 
 footer.append(footerContainer);
 footerContainer.append(footerContacts, footerLocations, footerImg);
--- shelter/src/assets/js/main/section_about.js
+++ shelter/src/assets/js/main/section_about.js
@@ -1,24 +1,13 @@
+import { createElement } from '../common/createElement';
 import aboutImgSrc from '../../img/about-the-shelter.webp';
 
-export const sectionAbout = document.createElement('section');
-sectionAbout.classList.add('about');
-const aboutContainer = document.createElement('div');
-aboutContainer.classList.add('about__container');
-const aboutImg = document.createElement('img');
-aboutImg.classList.add('about__img');
-aboutImg.alt = '';
-aboutImg.src = aboutImgSrc;
-const aboutBlock = document.createElement('div');
-aboutBlock.classList.add('about__text-block');
-const aboutTitle = document.createElement('h2');
-aboutTitle.classList.add('about__title');
-aboutTitle.innerHTML = `About the shelter<br>“Cozy House”`
-const aboutText1 = document.createElement('p');
-aboutText1.classList.add('about__text');
-aboutText1.textContent = 'Currently we have 121 dogs and 342 cats on our hands and statistics show that only 20% of them will find a family. The others will continue to live with us and will be waiting for a lucky chance to become dearly loved.';
-const aboutText2 = document.createElement('p');
-aboutText2.classList.add('about__text');
-aboutText2.textContent = 'We feed our wards with the best food and make sure that they do not get sick, feel comfortable (including psychologically) and well. We are supported by 87 volunteers and 28 employees of various skill levels. About 12% of the animals are taken by the shelter staff. Taking care of the animals, they become attached to the pets and would hardly ever leave them alone.';
+export const sectionAbout = createElement('section', ['about']);
+const aboutContainer = createElement('div', ['about__container']);
+const aboutImg = createElement('img', ['about__img'], '', { src: aboutImgSrc, alt: '' });
+const aboutBlock = createElement('div', ['about__text-block']);
+const aboutTitle = createElement('h2', ['about__title'], 'About the shelter “Cozy House”');
+const aboutText1 = createElement('p', ['about__text'], 'Currently we have 121 dogs and 342 cats on our hands and statistics show that only 20% of them will find a family. The others will continue to live with us and will be waiting for a lucky chance to become dearly loved.');
+const aboutText2 = createElement('p', ['about__text'], 'We feed our wards with the best food and make sure that they do not get sick, feel comfortable (including psychologically) and well. We are supported by 87 volunteers and 28 employees of various skill levels. About 12% of the animals are taken by the shelter staff. Taking care of the animals, they become attached to the pets and would hardly ever leave them alone.');
 
 sectionAbout.append(aboutContainer);
 aboutContainer.append(aboutImg, aboutBlock);
